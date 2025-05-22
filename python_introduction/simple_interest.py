7fb98c49bb732275d294442a2cff9906d950317b
MaggieNush <maggienungari.mn@gmail.com>
2025-05-22 07:47:22 +0300

    Add simple interest script

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,8 +2,7 @@ principal = 1000
 rate = 0.05
 time = 3
 
-simple_interest = (principal * rate * time)
+interest = (principal * rate * time)
 
-interest = simple_interest
 
-print(f"The simple interest is: {interest}")
+print(f"The simple interest is: {float(interest)}")
