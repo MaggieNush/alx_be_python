6de567126edb7d6165d24d31f335c5eb52f91fe4
MaggieNush <maggienungari.mn@gmail.com>
2025-06-15 18:08:45 +0300

    Add a library management file

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,55 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    def check_out(self):
+        if self._is_checked_out:
+            return False
+        else :
+            self._is_checked_out = True
+            return True
+        
+    def return_book(self):
+        if self._is_checked_out :
+            self._is_checked_out = False
+            return True
+        
+        else:
+            return False
+        
+class Library:
+    def __init__(self):
+        self._books = []
+
+    def add_book(self, book):
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        for book in self._books:
+            if book.title == title:
+                return book.check_out()
+        return False
+    
+    def return_book(self, title):
+        for book in self._books:
+            if book.title == title:
+                return book.return_book()
+        return False
+    
+    def list_available_books(self):
+        for book in self._books:
+            if book._is_checked_out == False:
+        
+             print(f"{book.title} by {book.author}")
+        
+
+
+
+
+            
+            
+
+    
+    
